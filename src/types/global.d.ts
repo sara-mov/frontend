53dcfc5861ab57feeeb33ce60ac6f8a142ef265f
Amaan <amaanshaikh.gg@gmail.com>
2025-03-04 22:24:37 +0530

    new: speech recognition

---
+++ src/types/global.d.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export {};
+
+declare global {
+  interface Window {
+    SpeechRecognition: typeof SpeechRecognition;
+    webkitSpeechRecognition: typeof SpeechRecognition;
+  }
+
+  interface SpeechRecognition extends EventTarget {
+    continuous: boolean;
+    interimResults: boolean;
+    lang: string;
+    start(): void;
+    stop(): void;
+    onresult: (event: SpeechRecognitionEvent) => void;
+    onerror: (event: SpeechRecognitionErrorEvent) => void;
+    onstart: () => void;
+    onend: () => void;
+  }
+
+  interface SpeechRecognitionEvent extends Event {
+    error: any;
+    results: SpeechRecognitionResultList;
+    resultIndex: number;
+  }
+}
