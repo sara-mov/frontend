51799874778bcaa841b0e9abf0e7c4fad4e5105f
Amaan <amaanshaikh.gg@gmail.com>
2025-03-07 23:04:15 +0530

    new: line-clamp component

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const ExpandableText = ({ text }: { text: string }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <div
+      className={`w-[50%] max-w-full text-justify cursor-pointer transition-all duration-300 ${
+        isExpanded ? "" : "line-clamp-3 overflow-hidden"
+      }`}
+      onClick={() => setIsExpanded(!isExpanded)}
+    >
+      {text}
+    </div>
+  );
+};
+
+export default ExpandableText;
