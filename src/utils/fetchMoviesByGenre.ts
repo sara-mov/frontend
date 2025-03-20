feffa38c3e501b831a46fc951a002ba8c36fbbb9
Amaan <amaanshaikh.gg@gmail.com>
2025-03-20 22:36:22 +0530

    update: data fetch

--- src/utils/fetchMoviesByGenre.ts
+++ src/utils/fetchMoviesByGenre.ts
@@ -4,8 +4,10 @@ export async function fetchMoviesByGenre(genre: string) {
     console.log(genre)
     const { data, error } = await supabase
       .from('movies')
+      // .from('hi_mr_movies')
       .select('*')
       .contains('genres', [genre])
+      // .in('original_language', ['hi', 'en'])
       // .order('popularity', { ascending: false })
       .limit(50);
   
@@ -13,8 +15,6 @@ export async function fetchMoviesByGenre(genre: string) {
       console.error('Error fetching movies:', error);
       return [];
     }
-
-    console.log(data)
   
     return data;
   }
