583749c0ff8d71ceafa792c63cae9763cb83ca0c
Amaan <amaanshaikh.gg@gmail.com>
2025-03-22 00:29:35 +0530

    fix: api

--- src/app/api/movies/more-detail/[imdb_id]/route.tsx
+++ src/app/api/movies/more-detail/[imdb_id]/route.tsx
@@ -1,25 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { imdb_id: string } }
-) {
+export async function GET(req: NextRequest) {
   try {
-    const imdb_id = params.imdb_id; 
+    const url = new URL(req.url);
+    const imdb_id = url.pathname.split("/").pop(); // Extract IMDb ID from URL
 
     if (!imdb_id) {
       return NextResponse.json({ error: "Invalid IMDb ID" }, { status: 400 });
     }
 
     const response = await fetch(
-      ` http://www.omdbapi.com/?i=${imdb_id}&apikey=${process.env.NEXT_PUBLIC_OMDB_API_KEY}`
+      `http://www.omdbapi.com/?i=${imdb_id}&apikey=${process.env.NEXT_PUBLIC_OMDB_API_KEY}`
     );
     const data = await response.json();
 
-    return Response.json(data);
+    return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json(
-      { error: `Failed to fetch movie details ${error}` },
+      { error: `Failed to fetch movie details: ${error}` },
       { status: 500 }
     );
   }
