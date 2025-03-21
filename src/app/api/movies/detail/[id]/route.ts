377c1adc5af8092f953033edfc95ebdbf934193a
Amaan <amaanshaikh.gg@gmail.com>
2025-03-22 00:20:37 +0530

    fix: api context

---
+++ src/app/api/movies/detail/[id]/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  const id = Number(context.params.id);
+
+  if (isNaN(id)) {
+    return NextResponse.json({ error: "Invalid movie ID" }, { status: 400 });
+  }
+
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US`
+    );
+    const data = await response.json();
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: `Failed to fetch data: ${error}` },
+      { status: 500 }
+    );
+  }
+}
